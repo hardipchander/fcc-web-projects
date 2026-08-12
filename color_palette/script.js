3226fd768d9a95e989e5ed2bce29db11b75b8198
hardipchander <hardipchander@gmail.com>
2026-08-12 14:15:30 -0400

    feat(color_palette): add javascript behaviour to web page so it generates random colors and then displays them onto the screen

--- color_palette/script.js
+++ color_palette/script.js
@@ -1,1 +1,38 @@
-// 130:00
+const generateBtn=document.getElementById("generate-btn");
+const paletteContainer=document.querySelector(".palette-container");
+
+generateBtn.addEventListener("click", generatePalette);
+
+function generatePalette() {
+    const colors=[];
+    for(let i=0; i<5; i++) {
+        colors.push(generateRandomColor());
+    }
+    updatePaletteDisplay(colors);
+}
+
+function generateRandomColor() {
+    const letters="0123456789ABCDEF";
+    let color="#";
+
+    for(let i=0;i<6; i++) {
+        color+=letters[Math.floor(Math.random() *letters.length)];
+    }
+
+    return color;
+}
+
+function updatePaletteDisplay(colorsArr) {
+    const colorBoxes=document.querySelectorAll(".color-box"); 
+
+    colorBoxes.forEach((box, index) => {
+        const color=colorsArr[index];
+        const colorDiv=box.querySelector(".color");
+        const hexValue=box.querySelector(".hex-value");
+
+        colorDiv.style.backgroundColor=color;
+        hexValue.textContent=color;
+    });
+}
+
+generatePalette();
